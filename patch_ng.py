e75df285f040abe7fcafbacf96f37da1114ad031
Uilian Ries <uilianr@jfrog.com>
2025-10-06 08:20:10 +0200

    Mark 1.19 as stable version
    Signed-off-by: Uilian Ries <uilianr@jfrog.com>

--- patch_ng.py
+++ patch_ng.py
@@ -29,7 +29,7 @@
     SOFTWARE.
 """
 __author__ = "Conan.io <info@conan.io>"
-__version__ = "1.19.0-dev"
+__version__ = "1.19.0"
 __license__ = "MIT"
 __url__ = "https://github.com/conan-io/python-patch"
 

b2f05c24b490b4f61a54c66839d9b25b28ae97d0
Uilian Ries <uilianr@jfrog.com>
2025-10-08 09:39:21 +0200

    Add CCI validation
    Signed-off-by: Uilian Ries <uilianr@jfrog.com>

---
+++ .github/scripts/validate-cci-patch-ng.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+SAMPLE_RECIPES_NUM=10
+
+# Find all conanfile.py files that use apply_conandata_patches
+RECIPES=$(find . -type f -name "conanfile.py" -exec grep "apply_conandata_patches(self)" {} + | sort | uniq | cut -d':' -f1)
+
+echo "Found $(echo "$RECIPES" | wc -l) recipes using apply_conandata_patches."
+
+# Pick 10 random recipes
+SAMPLE_RECIPES=$(shuf -e ${RECIPES[@]} -n $SAMPLE_RECIPES_NUM)
+
+echo "Pick $SAMPLE_RECIPES_NUM random recipes to test:"
+echo "$SAMPLE_RECIPES"
+
+# Run conan create for each sampled recipe
+for it in $SAMPLE_RECIPES; do
+    recipe_dir=$(dirname "${it}")
+    pushd "$recipe_dir"
+    echo "Testing recipe in directory: ${recipe_dir}"
+    # Get a version from conandata.yml that uses a patch
+    version=$(yq '.patches | keys | .[0]' conandata.yml 2>/dev/null)
+    if [ -z "$version" ]; then
+        echo "ERROR: No patches found in conandata.yml for $recipe_dir, skipping."
+        continue
+    fi
+    version=$(echo ${version} | tr -d '"')
+    # Replace apply_conandata_patches to exit just after applying patches
+    sed -i -e 's/apply_conandata_patches(self)/apply_conandata_patches(self); import sys; sys.exit(0)/g' conanfile.py
+    # Create the package with the specified version
+    conan create . --version=${version} --build=missing
+    popd
+done
